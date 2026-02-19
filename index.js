d153315becb40b624cfd260f4d32b1cabf9f4b52
rukaiahramisa53 <rukaiah.ramisa@gmail.com>
2026-02-19 07:45:18 +0600

    navbar all done and responsive

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+const loadProductDetails=async(id)=>{
+    const url=`https://fakestoreapi.com/products/${id}`;
+    const response=await fetch(url);
+    const details= await response.json();
+    displayProductDetails(details);
+}
+
+const displayProductDetails=(product)=>{
+    console.log(product);
+    const detailsBox= document.getElementById("detailsContainer");
+    detailsBox.innerHTML=`
+          <div class="p-4 space-y-3">
+
+  <img src="${product.image}" alt="" class="h-48 rounded-lg mx-auto">
+
+  <h2 class="text-xl font-semibold text-black">${product.title}
+    
+  </h2>
+  <div class="flex justify-between items-center">
+
+    <span class="text-lg font-semibold text-black">
+      $${product.price}
+    </span>
+
+  
+    <div class="flex flex-col items-end">
+      <span class="flex items-center gap-1 text-black text-sm">
+        <i class="fa-solid fa-star text-yellow-500"></i>${product.rating.rate}
+      </span>
+      <span class="text-xs text-black">${product.rating.count} Reviews</span>
+    </div>
+
+  </div>
+
+  <p class="text-black text-sm">${product.description}</p>
+
+
+  <button class="bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded-md text-sm font-medium">
+    Add to Cart
+  </button>
+
+</div> `;
+    document.getElementById("my_modal_5").showModal();
+} 
+
+
 const loadTrendingProducts=()=>{
     fetch("https://fakestoreapi.com/products")
     .then(res=>res.json())
@@ -21,7 +67,7 @@ const loadTrendingProducts=()=>{
               <span class="badge badge-outline text-primary">
                 ${product.category}
               </span>
-              <span class="text-yellow-500"> <i class="fa-solid fa-star"></i> ${product.rating.rate} (${product.rating.count})
+              <span class="text-yellow-500"> <i class="fa-solid fa-star"></i> ${product.rating.rate} ${product.rating.count}
               </span>
             </div>
 
@@ -50,21 +96,26 @@ const loadTrendingProducts=()=>{
     });
 };
 
+
 loadTrendingProducts();
             
-function setActive(activeId) {
+function setActive(activeIdDesktop,activeIdMobile) {
+      [
+    "homeBtn","productsBtn","aboutBtn","contactBtn",
+    "homeBtnSmall","productsBtnSmall","aboutBtnSmall","contactBtnSmall"
+  ].forEach(id => {
+      document.getElementById(id).classList.remove("active-nav");
+  });
 
-  document.getElementById("homeBtn").classList.remove("active-nav");
-  document.getElementById("productsBtn").classList.remove("active-nav");
-
-  document.getElementById(activeId).classList.add("active-nav");
+  document.getElementById(activeIdDesktop).classList.add("active-nav");
+  document.getElementById(activeIdMobile).classList.add("active-nav");
 }
 
 function showHome() {
   document.getElementById("Home").classList.remove("hidden");
   document.getElementById("Products").classList.add("hidden");
 
-  setActive("homeBtn");
+  setActive("homeBtn","homeBtnSmall");
   window.scrollTo(0, 0);
 }
 
@@ -72,12 +123,28 @@ function showProducts() {
   document.getElementById("Home").classList.add("hidden");
   document.getElementById("Products").classList.remove("hidden");
 
-  setActive("productsBtn");
+  setActive("productsBtn","productsBtnSmall");
   window.scrollTo(0, 0);
 }
+
 window.onload = function() {
-  setActive("homeBtn");
+  setActive("homeBtn","homeBtnSmall");
 };
+
+function showWorkingPage(type){
+    document.getElementById("Home").classList.add("hidden");
+    document.getElementById("Products").classList.add("hidden");
+    document.getElementById("workingPage").classList.remove("hidden");
+    if(type === "about"){
+      setActive("aboutBtn","aboutBtnSmall");
+  }
+  else if(type === "contact"){
+      setActive("contactBtn","contactBtnSmall");
+  }
+
+  window.scrollTo(0,0);
+}
+
 let allProducts=[];
 
 const loadProducts=()=>{
@@ -116,7 +183,7 @@ loadProducts();
         </p>
 
         <div class="flex gap-3 mt-3">
-          <button class="btn btn-outline btn-sm flex-1" onclick="my_modal_5.showModal()">
+          <button class="btn btn-outline btn-sm flex-1" onclick="loadProductDetails(${product.id})">
             Details
           </button>
           <button class="btn btn-primary btn-sm flex-1">
@@ -148,12 +215,12 @@ const buttons = document.querySelectorAll(".cat-btn");
 
 buttons.forEach(btn => {
   btn.addEventListener("click", () => {
-    // Reset all buttons
+
     buttons.forEach(b => {
       b.classList.remove("bg-white", "text-[#6366f1]");
       b.classList.add("bg-white", "text-black");
     });
-    // Set clicked button as active
+   
     btn.classList.remove("bg-white", "text-black");
     btn.classList.add("bg-white", "text-[#6366f1]");
   });
